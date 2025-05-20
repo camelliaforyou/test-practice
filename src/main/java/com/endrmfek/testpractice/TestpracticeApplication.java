c1f4dc8cad1445618893860737c6ab0f8b556afa
endrmfek <alstn9469@naver.com>
2025-05-20 20:47:39 +0900

    Initial commit

---
+++ src/main/java/com/endrmfek/testpractice/TestpracticeApplication.java
@@ -0,0 +1,13 @@
+package com.endrmfek.testpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestpracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestpracticeApplication.class, args);
+    }
+
+}
